e864233ce1e0da78edeefea56c1c998adc4e92e0
Johan Ronsse <johan.ronsse@gmail.com>
2015-12-23 14:50:42 +0100

    Fix some basics

---
+++ content/data/clients.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: "Acme Inc."
+  },
+  {
+    name: "Something Inc."
+  },
+  {
+    name: "Something Inc."
+  },
+  {
+    name: "Acme Inc."
+  },
+  {
+    name: "Something Inc."
+  },
+  {
+    name: "Something Inc."
+  }
+];
