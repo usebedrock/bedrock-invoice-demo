3c571cea701b02cdb6592b7ef19f3cad058098f1
Johan Ronsse <johan.ronsse@gmail.com>
2016-03-17 08:34:45 +0100

    Update core

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const gulp = require('gulp');
+const inquirer = require('inquirer');
 
 const browserSync = require('./core/tasks/browser-sync');
 const sass = require('./core/tasks/sass');
@@ -12,7 +13,7 @@ const watch = require('./core/tasks/watch');
 const server = require('./core/tasks/server');
 const modernizr = require('./core/tasks/modernizr');
 
-gulp.task('sass', sass);
+gulp.task('sass', ['icon-font'], sass);
 gulp.task('modernizr', modernizr);
 gulp.task('server', server);
 gulp.task('copy:images', copy.images);
@@ -22,18 +23,34 @@ gulp.task('copy:resources', copy.resources);
 gulp.task('bundle', bundle);
 gulp.task('icon-font', iconFont);
 
-gulp.task('templates:clean', templates.clean);
-gulp.task('templates:compile', ['templates:clean', 'templates:compile:content', 'templates:compile:styleguide']);
+gulp.task('templates:compile', ['templates:compile:content', 'templates:compile:styleguide', 'templates:compile:docs']);
 gulp.task('templates:compile:content', templates.compile.content);
 gulp.task('templates:compile:styleguide', templates.compile.styleguide);
+gulp.task('templates:compile:docs', templates.compile.docs);
 
 gulp.task('watch', watch);
 
 gulp.task('copy', ['copy:images', 'copy:fonts', 'copy:resources', 'copy:favicon']);
 gulp.task('compile-all', ['modernizr', 'icon-font', 'bundle', 'sass', 'copy']);
-gulp.task('build', ['compile-all', 'templates:compile'], function () {
+gulp.task('build', ['compile-all', 'templates:compile'], function (done) {
+  console.log('------------\n');
   console.log('Build finished. Compiled files can be found in the dist/ directory.');
-  process.exit(0);
+  inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'clean',
+      message: 'Would you like to clean the dist/ directory?',
+      default: true
+    }
+  ], function (answers) {
+    if (answers.clean) {
+      templates.clean(function () {
+        process.exit(0);
+      });
+    } else {
+      process.exit(0);
+    }
+  });
 });
 gulp.task('browser-sync', ['server', 'compile-all', 'watch'], browserSync);
 gulp.task('default', ['browser-sync']);
