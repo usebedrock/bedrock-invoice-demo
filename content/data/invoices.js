633353112771e2e20c53a47e310f81279c2e40c0
Johan Ronsse <johan.ronsse@gmail.com>
2016-03-17 08:34:53 +0100

    More invoice data

--- content/data/invoices.js
+++ content/data/invoices.js
@@ -40,5 +40,47 @@ module.exports = [
     date: "December 12",
     value: "10.000",
     currency: "€"
+  },
+  {
+    name: "Acme Inc.",
+    id: "4",
+    date: "December 10",
+    value: "20.000",
+    currency: "€"
+  },
+  {
+    name: "Something Inc.",
+    id: "5",
+    date: "December 11",
+    value: "1.000",
+    currency: "€"
+  },
+  {
+    name: "Something Inc.",
+    id: "6",
+    date: "December 12",
+    value: "10.000",
+    currency: "€"
+  },
+  {
+    name: "Acme Inc.",
+    id: "4",
+    date: "December 10",
+    value: "20.000",
+    currency: "€"
+  },
+  {
+    name: "Something Inc.",
+    id: "5",
+    date: "December 11",
+    value: "1.000",
+    currency: "€"
+  },
+  {
+    name: "Something Inc.",
+    id: "6",
+    date: "December 12",
+    value: "10.000",
+    currency: "€"
   }
 ];
